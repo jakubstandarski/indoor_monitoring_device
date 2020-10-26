e354d707b4d669b8be7d2a25d7173592c15848f4
Jakub Standarski <jstand.jakub.standarski@gmail.com>
2020-10-26 17:31:23 +0100

    New directory for peripherals source and header files added.
    "peripherals" directory filled in with two files:
    - system_clock.c ("source" directory);
    - system_clock.h ("include" directory).
    They are only initialized with basic template.

---
+++ peripherals/include/system_clock.h
@@ -0,0 +1,15 @@
+/*
+ * Author: Jakub Standarski
+ * Email: jstand.jakub.standarski@gmail.com
+ *
+ * Date: 26.10.2020
+ *
+ */
+
+#ifndef SYSTEM_CLOCK_H
+    #define SYSTEM_CLOCK_H
+
+
+
+#endif /*SYSTEM_CLOCK_H */
+
---
+++ peripherals/source/system_clock.c
@@ -0,0 +1,8 @@
+/*
+ * Author: Jakub Standarski
+ * Email: jstand.jakub.standarski@gmail.com
+ *
+ * Date: 26.10.2020
+ *
+ */
+
