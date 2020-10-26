5c8796609ef369f511782017d9213f833439366a
Jakub Standarski <jstand.jakub.standarski@gmail.com>
2020-10-26 18:18:43 +0100

    System clock initialization public function prototype added.
    "system_clock_init()" public function prototype and its description
    added.

--- peripherals/include/system_clock.h
+++ peripherals/include/system_clock.h
@@ -9,6 +9,21 @@
 #ifndef SYSTEM_CLOCK_H
     #define SYSTEM_CLOCK_H
 
+/*****************************************************************************/
+/* PUBLIC FUNCTIONS PROTOTYPES */
+/*****************************************************************************/
+
+/**
+ * @brief   Set clock source (i.e. HSI or HSE), system clock source and its
+ *          speed if PLL is used (HCLK), AHB1 prescaler, APB1 prescaler,
+ *          APB2 prescaler and Flash latency.
+ *
+ * @param   None.
+ *
+ * @retval  None.
+ */
+void system_clock_init(void);
+
 
 
 #endif /*SYSTEM_CLOCK_H */
