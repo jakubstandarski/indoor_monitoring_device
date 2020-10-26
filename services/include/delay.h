787c02718fbf485832a5a958eeff74860a457f44
Jakub Standarski <jstand.jakub.standarski@gmail.com>
2020-10-26 20:01:45 +0100

    Header file "delay.h" filled in with basic API.
    "delay.h" provides so far two functions and its descriptions:
    - "delay_timer_init()";
    - "delay_ms()".

--- services/include/delay.h
+++ services/include/delay.h
@@ -9,6 +9,38 @@
 #ifndef DELAY_H
     #define DELAY_H
 
+/*****************************************************************************/
+/* HEADERS */
+/*****************************************************************************/
+
+#include <stdint.h>
+
+
+
+/*****************************************************************************/
+/* PUBLIC FUNCTIONS PROTOTYPES */
+/*****************************************************************************/
+
+/**
+ * @brief   Initialize timer responsible of delay functions.
+ *
+ * @param   None.
+ *
+ * @retval  None.
+ */
+void delay_timer_init(void);
+
+
+
+/**
+ * @brief   Wait given amount of milliseconds (blocking function).
+ *
+ * @param   milliseconds - delay time given in milliseconds.
+ *
+ * @retval  None.
+ */
+void delay_ms(uint32_t milliseconds);
+
 
 
 #endif /* DELAY_H */
