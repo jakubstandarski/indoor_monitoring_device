e7d0b576af0fe8854ae03da5af67358c61d49e94
Jakub Standarski <jstand.jakub.standarski@gmail.com>
2020-10-28 19:32:57 +0100

    "circular_buffer_status_t" public enumeration added.

--- services/include/circular_buffer.h
+++ services/include/circular_buffer.h
@@ -9,6 +9,19 @@
 #ifndef CIRCULAR_BUFFER_H
     #define CIRCULAR_BUFFER_H
 
+/*****************************************************************************/
+/* PUBLIC ENUMS */
+/*****************************************************************************/
+
+typedef enum circular_buffer_status {
+    CIRCULAR_BUFFER_STATUS_OK,
+    CIRCULAR_BUFFER_STATUS_ERROR,
+    CIRCULAR_BUFFER_STATUS_EMPTY,
+    CIRCULAR_BUFFER_STATUS_NOT_EMPTY,
+    CIRCULAR_BUFFER_STATUS_FULL,
+    CIRCULAR_BUFFER_STATUS_NOT_FULL
+}circular_buffer_status_t;
+
 
 
 
