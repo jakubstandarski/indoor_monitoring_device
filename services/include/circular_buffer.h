5de6abad1581c37968aa3efcd6ab23286de158eb
Jakub Standarski <jstand.jakub.standarski@gmail.com>
2020-10-28 19:28:48 +0100

    Empty header file "circular_buffer.h" added.

---
+++ services/include/circular_buffer.h
@@ -0,0 +1,16 @@
+/*
+ * Author: Jakub Standarski
+ * Email: jstand.jakub.standarski@gmail.com
+ *
+ * Date: 28.10.2020
+ *
+ */
+
+#ifndef CIRCULAR_BUFFER_H
+    #define CIRCULAR_BUFFER_H
+
+
+
+
+#endif /* CIRCULAR_BUFFER_H */
+
