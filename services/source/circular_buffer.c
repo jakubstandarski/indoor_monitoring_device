188f798238cd5753f9987379063b8c9685b7e56d
Jakub Standarski <jstand.jakub.standarski@gmail.com>
2020-10-28 19:54:32 +0100

    "circular_buffer_init()" public function definition added.

--- services/source/circular_buffer.c
+++ services/source/circular_buffer.c
@@ -16,3 +16,19 @@
 
 
 
+/*****************************************************************************/
+/* PUBLIC FUNCTIONS DEFINITIONS */
+/*****************************************************************************/
+
+circular_buffer_status_t circular_buffer_init(
+    circular_buffer_t *circular_buffer
+)
+{
+    memset(circular_buffer->buffer, 0, CIRCULAR_BUFFER_SIZE);
+    circular_buffer->head = 0;
+    circular_buffer->tail = 0;
+    circular_buffer->items_count = 0;
+
+    return CIRCULAR_BUFFER_STATUS_OK;
+}
+
