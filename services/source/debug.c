97e52f5b29f4033848d6c35f6ce29e136d981c6e
Jakub Standarski <jstand.jakub.standarski@gmail.com>
2020-10-29 09:16:56 +0100

    "debug.c" updated with header files.

--- services/source/debug.c
+++ services/source/debug.c
@@ -6,3 +6,16 @@
  *
  */
 
+/*****************************************************************************/
+/* HEADERS */
+/*****************************************************************************/
+
+#include "debug.h"
+#include "circular_buffer.h"
+
+#include "stm32f4xx_ll_bus.h"
+#include "stm32f4xx_ll_gpio.h"
+#include "stm32f4xx_ll_usart.h"
+
+#include <string.h>
+
