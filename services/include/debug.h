4d778c786e2a873bc97fd128920da7f42bbe14af
Jakub Standarski <jstand.jakub.standarski@gmail.com>
2020-10-29 09:08:18 +0100

    Empty header file "debug.h" added.

---
+++ services/include/debug.h
@@ -0,0 +1,16 @@
+/*
+ * Author: Jakub Standarski
+ * Email: jstand.jakub.standarski@gmail.com
+ *
+ * Date: 29.10.2020
+ *
+ */
+
+#ifndef DEBUG_H
+    #define DEBUG_H
+
+
+
+
+#endif /* DEBUG_H */
+
