8b6b23b81e765b82905cc3d1dfa9ff89351f4b2c
Jakub Standarski <jstand.jakub.standarski@gmail.com>
2020-10-29 09:21:10 +0100

    Private function definition "debug_uart_gpio_init()" added.

--- services/source/debug.c
+++ services/source/debug.c
@@ -19,3 +19,32 @@
 
 #include <string.h>
 
+
+
+/*****************************************************************************/
+/* PRIVATE FUNCTIONS PROTOTYPES */
+/*****************************************************************************/
+
+static void debug_uart_gpio_init(void);
+
+
+
+/*****************************************************************************/
+/* PRIVATE FUNCTIONS DEFINITIONS */
+/*****************************************************************************/
+
+static void debug_uart_gpio_init(void)
+{
+    LL_AHB1_GRP1_EnableClock(DEBUG_UART_GPIO_PORT_CLOCK);
+
+    LL_GPIO_InitTypeDef uart_gpio_settings = {
+        .Pin = DEBUG_UART_GPIO_PIN_TX,
+        .Mode = LL_GPIO_MODE_ALTERNATE,
+        .Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH,
+        .OutputType = LL_GPIO_OUTPUT_PUSHPULL,
+        .Pull = LL_GPIO_PULL_NO,
+        .Alternate = LL_GPIO_AF_7
+    };
+    LL_GPIO_Init(DEBUG_UART_GPIO_PORT, &uart_gpio_settings);
+}
+
