dbe996c50ecffa7da26bddc490bd5e42475c2276
Jakub Standarski <jstand.jakub.standarski@gmail.com>
2020-11-05 22:39:37 +0100

    "pwr_clock_init()" private function added.

--- system/source/pwr.c
+++ system/source/pwr.c
@@ -15,3 +15,22 @@
 #include "stm32f4xx_ll_bus.h"
 #include "stm32f4xx_ll_pwr.h"
 
+
+
+/*****************************************************************************/
+/* PRIVATE FUNCTIONS PROTOTYPES */
+/*****************************************************************************/
+
+static void pwr_clock_init(void);
+
+
+
+/*****************************************************************************/
+/* PRIVATE FUNCTIONS DEFINITIONS */
+/*****************************************************************************/
+
+static void pwr_clock_init(void)
+{
+    LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+}
+
