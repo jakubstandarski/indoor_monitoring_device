d4789f4b957702968109de77b4add21a5d8c7e4a
Jakub Standarski <jstand.jakub.standarski@gmail.com>
2020-11-05 22:35:15 +0100

    "pwr_init()" public function prototype added.

--- system/include/pwr.h
+++ system/include/pwr.h
@@ -9,6 +9,20 @@
 #ifndef PWR_H
     #define PWR_H
 
+/*****************************************************************************/
+/* PUBLIC FUNCTIONS PROTOTYPES */
+/*****************************************************************************/
+
+/**
+ * @brief   Enable power controller, its clock and backup domain access.
+ *          Set regulation voltage scalling output.
+ *
+ * @param   None.
+ *
+ * @retval  None.
+ */
+void pwr_init(void);
+
 
 
 #endif /* PWR_H */
