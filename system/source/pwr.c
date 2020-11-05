1f7a1e149ad77d60ecb810e8884d312755d4b7df
Jakub Standarski <jstand.jakub.standarski@gmail.com>
2020-11-05 22:47:10 +0100

    "pwr_peripheral_init()" private function definition added.

--- system/source/pwr.c
+++ system/source/pwr.c
@@ -22,6 +22,7 @@
 /*****************************************************************************/
 
 static void pwr_clock_init(void);
+static void pwr_peripheral_init(void);
 
 
 
@@ -32,5 +33,20 @@ static void pwr_clock_init(void);
 static void pwr_clock_init(void)
 {
     LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+}
+
+
+
+static void pwr_peripheral_init(void)
+{
+    LL_PWR_SetRegulVoltageScaling(LL_PWR_REGU_VOLTAGE_SCALE1);
+    while (LL_PWR_GetRegulVoltageScaling() != LL_PWR_REGU_VOLTAGE_SCALE1) {
+        ;
+    }
+
+    LL_PWR_EnableBkUpAccess();
+    while (LL_PWR_IsEnabledBkUpAccess() != 1) {
+        ;
+    }
 }
 
